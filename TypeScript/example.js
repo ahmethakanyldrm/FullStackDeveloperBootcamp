c10e1cb94e2a390bc896698fed428fa1b37cec8f
ahmethakanyldrm <hakan-336-ahmet@outlook.com>
2024-04-07 00:06:10 +0300

    Jquery ve TypeScript

---
+++ TypeScript/example.js
@@ -0,0 +1,4 @@
+console.log("Hello World 2");
+console.log(2 + 2);
+var isim = "Abc";
+console.log(isim);
---
+++ TypeScript/example.ts
@@ -0,0 +1,6 @@
+console.log("Hello World 2");
+
+console.log(2+2);
+
+let isim: string = "Abc";
+console.log(isim);
