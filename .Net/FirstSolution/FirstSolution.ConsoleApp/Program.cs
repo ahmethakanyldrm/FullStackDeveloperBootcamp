c0eccb9c0af05285cdfda201992b12ddf6d15bc9
ahmethakanyldrm <hakan-336-ahmet@outlook.com>
2024-04-21 00:00:00 +0300

    C# Backend Start
    C# Değişkenler, Şart Blokları, Döngüler, Goto

---
+++ .Net/FirstSolution/FirstSolution.ConsoleApp/Program.cs
@@ -0,0 +1,144 @@
+﻿namespace FirstSolution.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Değişkenler
+
+
+            // değişkenler
+            // string, int, bool, decimal, double, float, object, DateTime, short, Long, char
+
+            string name = "Ahmet Hakan ";
+            string lastName = "Yıldırım";
+
+            string message = "Hello " + name + lastName;
+            string message2 = $"Hello {name} {lastName}";
+            string message3 = @$"Hello {name} {lastName} \";
+            Console.WriteLine(message);
+            Console.WriteLine(message2);
+
+            int num = 0;
+            Console.WriteLine(num);
+
+            int age = 24;
+
+            bool isActive = false;
+            decimal decimalNum = 0;
+            double doubleNum = 23.5;
+            float floatNum = 12.5f;
+
+
+            // boxing 
+            object firstName = "Ahmet Hakan";
+            // unboxing
+            Console.WriteLine((string)firstName);
+
+            // boxing 
+            object x = 0;
+            object y = 1;
+
+            // unboxing
+            var z = (int)x + (int)y;
+
+
+            DateTime date = DateTime.Now;
+            DateTime date2 = Convert.ToDateTime("20.04.2024");
+
+
+
+            #endregion
+
+            #region Şart Blokları
+
+            if (name == "Ahmet Hakan")
+            {
+
+                // bu işlemi yap
+
+            } else
+            {
+
+                if(age > 24)
+                {
+                    // bunu yap
+                } 
+                else if(age == 24)
+                {
+                    // 
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    // 
+                }
+
+            }
+
+            // ternary operatör => single if line
+            // şart ? // doğruysa bunu yap : // yanlış ise bunu yap
+            var result = (age >= 18 ? age++ : age--);
+
+            switch (age)    
+            {
+                case 10: Console.WriteLine("Giriş izni verildi");
+                    break;
+
+                case 20: Console.WriteLine("");
+                    break;
+
+                default: Console.WriteLine("");
+                    break;
+
+            }
+
+
+
+            #endregion
+
+            #region Döngüler
+
+            // for, foreach, while
+
+            for(int i = 0; i<10; i++)
+            {
+                // Console.WriteLine(i);
+            }
+
+            List<string> names = new () {
+                "Ahmet","Şule", "Emine", "Hakan"
+            };
+
+            string[] nameArray = ["", "",""];
+
+            foreach(string data in names)
+            {
+                if(data == "Ahmet" || data == "Hakan")
+                {
+                    Console.WriteLine("Seni Çok Seviyorum Şulem <3");
+                }else
+                {
+                    Console.WriteLine(data);
+                }
+
+            }
+
+            #endregion
+
+            #region Goto
+            tekrar:;
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+
+            if (c > 3) {
+                goto tekrar;
+           }
+            #endregion
+
+           
+        }
+    }
+}
---
+++ .Net/FirstSolution/FirstSolution.SayiTahminOyunu.ConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace FirstSolution.SayiTahminOyunu.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Merhaba Sayı tahmin oyununa hoşgeldin. Ben NumberAI");
+
+            int number = new Random().Next(1,100);
+            int answerNumber = 0;
+            while (number != answerNumber)
+            {
+                Console.WriteLine("1-100 arasında bir sayı tuttum. Bunu tahmin edebilir misin?");
+                Console.WriteLine("Sayı Giriniz:");
+                Console.ForegroundColor = ConsoleColor.White;
+                string? answer = Console.ReadLine();
+
+                int.TryParse(answer, out answerNumber);
+
+                if (number == answerNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Tebrikler tuttuğum sayıyı bildin!");
+                    
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Üzgünüm tuttuğum sayıyı bilemedin !");
+
+                }
+            }
+
+        }
+
+        private void tahmin()
+        {
+            Console.WriteLine("Merhaba Sayı tahmin oyununa hoşgeldin. Ben NumberAI");
+            tekrar:;
+            Console.WriteLine("1-100 arasında bir sayı tuttum. Bunu tahmin edebilir misin?");
+            Console.WriteLine("Sayı Giriniz:");
+            string? answer = Console.ReadLine();
+
+            int number = 10;
+            int.TryParse(answer, out int answerNumber);
+
+            if (number == answerNumber)
+            {
+                Console.WriteLine("Tebrikler tuttuğum sayıyı bildin!");
+            }
+            else
+            {
+                Console.WriteLine("Üzgünüm tuttuğum sayıyı bilemedin !");
+                goto tekrar;
+            }
+        }
+    }
+}
